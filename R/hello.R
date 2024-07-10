3e1c9112c3e632dfc56ac248d75cfd64a53be5c9
yzohdy <130336661+yzohdy@users.noreply.github.com>
2024-07-09 21:45:27 -0400

    Initial commit

---
+++ R/hello.R
@@ -0,0 +1,16 @@
+#' @title Hello function
+#'
+#' @description
+#' This function does things
+#'
+#'
+#' @param x
+#'
+#' @return Hell to name
+#' @export
+#'
+#' @examples
+#' hello(youssef)
+hello <- function(x) {
+  print(paste0("Hello",x, "world!"))
+}
---
+++ R/printer.R
@@ -0,0 +1,13 @@
+#' @title Printer
+#'
+#'@description Hi this is the printer fuction
+#'
+#' @param x
+#'
+#' @return hi
+#' @export
+#'
+#'
+printer <- function(x) {
+  paste("No",x)
+}
