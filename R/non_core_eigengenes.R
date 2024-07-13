1d62012d8b842d8f22c7337aa6318fd791f10630
yzohdy <130336661+yzohdy@users.noreply.github.com>
2024-07-12 22:35:29 -0400

    "New 1.1"

--- R/non_core_eigengenes.R
+++ R/non_core_eigengenes.R
@@ -5,8 +5,8 @@
 #' similar number to the rich-club nodes
 #'
 #' @param data microarray counts dataframe with rownames as genes and observations as columns
-#' @param study_network a study network igraph object
-#' @param rc_network a rich-club network igraph object
+#' @param study.net a study network igraph object
+#' @param core.nodes a rich-club network igraph object
 #' @param number number of sampling iterations to perform
 #'
 #' @return Dataframe with degrees
@@ -18,11 +18,11 @@
 #'
 #' @examples non_core_eigengenes(study_network, rc_network)
 
-non_core_eigengenes <- function(data, study_network, rc_network, number =100)
+non_core_eigengenes <- function(data, study.net, core.nodes, number =100)
 {
 
-  RC.genes <- as.data.frame(V(rc_network))
-  DEG.genes <- as.data.frame(V(study_network))
+  RC.genes <- as.data.frame(V(core.nodes))
+  DEG.genes <- as.data.frame(V(study.net))
 
   rownames(RC.genes) <- toupper(rownames(RC.genes))
   rownames(DEG.genes) <- toupper(rownames(DEG.genes))
@@ -30,18 +30,18 @@ non_core_eigengenes <- function(data, study_network, rc_network, number =100)
 
   genes.not.RC <- unique(DEG.genes[!(DEG.genes %in% rownames(RC.genes))])
 
-  rand.EG <- list(outcome)
+  rand.EG <- list()
   for (i in 1:number) {
     rand.groups <- data.frame(ID = c("a", "b"), Title = c("Rand", "All"))
-    rand.genes <- sample(genes.not.RC, size = nrow(RC.genes))
-    rand.gene.list <- list(a = c(paste(rand.genes)), b = c(paste(unique(DEGs))))
+    rand.genes <- sample(rownames(genes.not.RC), size = nrow(RC.genes))
+    rand.gene.list <- list(a = c(paste(rand.genes)), b = c(paste(unique(rownames(DEG.genes)))))
     rand.tmod <- makeTmodGS(gs2gene = rand.gene.list, gs = rand.groups)
     rand.tmod.set <- rand.tmod[grep("Rand", rand.tmod$gs$Title)]
     rand.eigv1 <- eigengene(data, g = rownames(data), mset = rand.tmod.set, k = 1)
     rand.eigv2 <- eigengene(data, g = rownames(data), mset = rand.tmod.set, k = 2)
     rand.eigv3 <- eigengene(data, g = rownames(data), mset = rand.tmod.set, k = 3)
     rand.egenes <- data.frame(a = t(rand.eigv1), b = t(rand.eigv2), c = t(rand.eigv3))
-}
-    return(rand.egenes)
+  }
+  return(rand.egenes)
 
 }
